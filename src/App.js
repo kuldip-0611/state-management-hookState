53ac6e96f7b3b219b4de666248262c881ee9247b
kuldip-zignuts <kuldipp@zignuts.com>
2023-04-13 15:45:18 +0530

    counter using hookState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { Button } from 'reactstrap';
 import './App.css';
+import {counterState} from './hookState';
+import { useHookstate } from '@hookstate/core';
+
 
 function App() {
+ 
+    const counter = useHookstate(counterState);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <div className='container bg-dark text-light p-3 w-25'>
+            {counter.get()}
+          </div>
+          <div className='buttons '>
+          <Button className='m-3' onClick={()=>counter.set(p=>p+1)}>Increment</Button>
+          <Button className='ms-3' onClick={()=>counter.set(p=>p-1)}>Decrement</Button>
+          <Button className='ms-3' onClick={()=>counter.set(0)}>Reset</Button>
+          </div>
     </div>
   );
 }
