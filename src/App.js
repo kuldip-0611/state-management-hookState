9ffc36f3a52ba670ff237a6fedcff198f95bc0e0
kuldip-zignuts <kuldipp@zignuts.com>
2023-04-17 10:52:52 +0530

    changes in ui

--- src/App.js
+++ src/App.js
@@ -58,19 +58,19 @@ function App() {
 
   return (
     <div className="App">
-      <div className='container'>
-        <h1>Total Money {counter.get().income - counter.get().expense} </h1>
+      <div className='container mt-3'>
+        <h1>Total Money <span className={`${(counter.get().income - counter.get().expense) >= 0 ? 'bg-success' : 'bg-danger'} rounded p-2`}>{counter.get().income - counter.get().expense}</span> </h1>
         <div className='row'>
           <div className='col'>
-            <h3>Add Expence {counter.get().expense} </h3>
-            <input type='text' value={expence} onChange={(e) => setExpence(e.target.value)} className='m-2' />
-            <input type='number' value={expenceValue} onChange={(e) => parseInt(setExpenceValue(e.target.value))} className='m-2' /> <br />
-            <Button onClick={handleExpence}>Expense</Button>
+            <h3>Add Expence <span className='bg-danger p-2 rounded text-light'> {counter.get().expense}</span> </h3>
+            <input type='text' value={expence} onChange={(e) => setExpence(e.target.value)} className='m-2' placeholder='discription' />
+            <input type='number' value={expenceValue} onChange={(e) => parseInt(setExpenceValue(e.target.value))} className='m-2' placeholder='value' /> <br />
+            <Button onClick={handleExpence} className='mb-3'>Add Expense</Button>
             {
 
               counter.get().expense_description.map((item,index) => (
                 <ListGroup key={index}>
-                  <ListGroupItem className='d-flex justify-content-around'>
+                  <ListGroupItem className='d-flex justify-content-around bg-danger text-light m-2'>
                     <span>{item.expence}
                     </span>
                     <span>{item.expenceValue}
@@ -86,15 +86,15 @@ function App() {
 
           </div>
           <div className='col'>
-            <h3>Add Money {counter.get().income}</h3>
-            <input type='text' value={income} onChange={(e) => setIncome(e.target.value)} className='m-2' />
-            <input type='number' value={money} onChange={(e) => parseInt(setMoney(e.target.value))} className='m-2' /> <br />
-            <Button onClick={handleIncome}>Add</Button>
+            <h3>Add Money <span className='bg-success text-light rounded p-2'>{counter.get().income}</span></h3>
+            <input type='text' value={income} onChange={(e) => setIncome(e.target.value)} className='m-2' placeholder='discription' />
+            <input type='number' value={money} onChange={(e) => parseInt(setMoney(e.target.value))} className='m-2' placeholder='value' /> <br />
+            <Button onClick={handleIncome} className='mb-3'>Add Money</Button>
             {
 
               counter.get().income_decription.map((item,index) => (
                 <ListGroup key={index}>
-                  <ListGroupItem className='d-flex justify-content-around'>
+                  <ListGroupItem className='d-flex justify-content-around bg-success text-light m-2'>
                     <span>{item.income}
                     </span>
                     <span>{item.money}
